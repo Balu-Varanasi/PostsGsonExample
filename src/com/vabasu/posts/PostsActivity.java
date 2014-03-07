214ee1db023d24d05cf965f516ecf5233a1bddd6
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2014-03-07 14:15:16 +0530

    initial-commit

---
+++ src/com/vabasu/posts/PostsActivity.java
@@ -0,0 +1,23 @@
+package com.vabasu.posts;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PostsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.posts, menu);
+        return true;
+    }
+    
+}
